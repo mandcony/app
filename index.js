857c0d8dcb4a65eea95beae24d713124cb44730e
maicol <luisminier79@gmail.com>
2024-12-31 12:23:24 -0500

    Your commit message here

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-registerRootComponent(App);
+export default function Home() {
+  return <App />;
+}
+
+registerRootComponent(Home);
